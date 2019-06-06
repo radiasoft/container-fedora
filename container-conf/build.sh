6f2d7ed3e2d7555a67486747cec2c76b588503e0
robnagler <bitbucket@q33.us>
2019-06-06 14:26:54 +0000

    add fpm here to make testing rpm-code easier

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -5,8 +5,10 @@ build_want_yum_update=1
 
 build_as_root() {
     install_repo_eval redhat-base
+    build_yum install ruby-devel
 }
 
 build_as_run_user() {
+    gem install --no-document fpm
     build_run_user_home_chmod_public
 }
