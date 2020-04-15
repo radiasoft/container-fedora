09b6713f479f663efbb5540489a0b765a14d8d8f
robnagler <github@q33.us>
2020-04-15 23:30:43 +0000

    remove build_run_user_home_chmod_public

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,9 +4,12 @@ build_is_public=
 build_want_yum_update=1
 
 build_as_root() {
+    umask 022
     install_repo_eval redhat-base
 }
 
 build_as_run_user() {
-    build_run_user_home_chmod_public
+    # Make sure all files in the run_user's home are public
+    # see radiasoft/download/installers/container-run
+    chmod -R a+rX "$build_run_user_home"
 }
