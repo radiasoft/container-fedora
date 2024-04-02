82497ca2c2ead3bef65a6106769749cf4874f26a
robnagler <github@q33.us>
2024-04-02 21:19:02 +0000

    Fix #4 unset all PYENV, PYTHON & VIRTUAL vars

--- container-conf/rsentry.sh
+++ container-conf/rsentry.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # Non-root users get the full {build_run_user_home} environment in the container.
 if (( $EUID != 0 )); then
-    # No core files. If someone wants a corefile, the can raise the limit.
+    # No core files. User can always raise this limit.
     ulimit -c 0
-    unset PYTHONPATH
-    unset PYTHONSTARTUP
+    unset $(compgen -A variable | egrep '^(PYTHON|PYENV|VIRTUAL)')
     export HOME={build_run_user_home}
     export PYENV_ROOT=$HOME/.pyenv
     source "$HOME"/.bashrc >& /dev/null
